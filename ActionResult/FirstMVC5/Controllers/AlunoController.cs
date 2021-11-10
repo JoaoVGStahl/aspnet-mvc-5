bf263db2509e26793136788a026ed2d3f53387b9
João Vitor Girardi <Jvitorstahl@hotmail.com>
2021-11-10 14:14:28 -0300

    Htmlhelpers with Razor

--- ActionResult/FirstMVC5/Controllers/AlunoController.cs
+++ ActionResult/FirstMVC5/Controllers/AlunoController.cs
@@ -7,20 +7,7 @@ namespace FirstMVC5.Controllers
     public class AlunoController : Controller
     {
         [Route("novo-aluno")]
-        public ActionResult Novo(Aluno aluno)
-        {
-            aluno = new Aluno()
-            {
-                Id = 1,
-                Nome = "João",
-                CPF = "12345678900",
-                DataMatricula = DateTime.Now,
-                Email = "Joao@Joao.com",
-                Ativo = true
-            };
-            return RedirectToAction("Index", aluno);
-        }
-        public ActionResult Index(Aluno aluno)
+        public ActionResult NovoAluno(Aluno aluno)
         {
             if (!ModelState.IsValid) return View(aluno);
 
