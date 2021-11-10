82bebf477d603850f68a027e33c7aaa262c98229
João Vitor Girardi <Jvitorstahl@hotmail.com>
2021-11-10 13:37:09 -0300

    Validacao de modelos

---
+++ ActionResult/FirstMVC5/Controllers/AlunoController.cs
@@ -0,0 +1,30 @@
+﻿using FirstMVC5.Models;
+using System;
+using System.Web.Mvc;
+
+namespace FirstMVC5.Controllers
+{
+    public class AlunoController : Controller
+    {
+        [Route("novo-aluno")]
+        public ActionResult Novo(Aluno aluno)
+        {
+            aluno = new Aluno()
+            {
+                Id = 1,
+                Nome = "João",
+                CPF = "12345678900",
+                DataMatricula = DateTime.Now,
+                Email = "Joao@Joao.com",
+                Ativo = true
+            };
+            return RedirectToAction("Index", aluno);
+        }
+        public ActionResult Index(Aluno aluno)
+        {
+            if (!ModelState.IsValid) return View(aluno);
+
+            return View(aluno);
+        }
+    }
+}
--- ActionResult/FirstMVC5/Models/Aluno.cs
+++ ActionResult/FirstMVC5/Models/Aluno.cs
@@ -1,7 +1,34 @@
-﻿namespace FirstMVC5.Models
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstMVC5.Models
 {
     public class Aluno
     {
-        prop
+        public int Id { get; set; }
+
+        [DisplayName("Nome Completo")]
+        [Required(ErrorMessage = "O campo {0} é requerido!")]
+        public string Nome { get; set; }
+
+        [DisplayName("E-mail")]
+        [Required(ErrorMessage = "O campo {0} é requerido!")]
+        [EmailAddress(ErrorMessage ="E-mail em formato inválido!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é requerido!")]
+        public string CPF { get; set; }
+
+        public DateTime DataMatricula { get; set; }
+
+        public bool Ativo { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é requerido!")]
+        public string Senha { get; set; }
+
+        [Compare("Senha", ErrorMessage ="As senha informadadas não conferem!")]
+        [Required(ErrorMessage = "O campo {0} é requerido!")]
+        public string SenhaConfirmacao { get; set; }
     }
 }
