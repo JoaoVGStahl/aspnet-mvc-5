ef63d710209b06c54f8a7608906dfafdad29a86a
João Vitor Girardi <Jvitorstahl@hotmail.com>
2021-11-11 08:35:44 -0300

    CSRF Protection

--- ActionResult/FirstMVC5/Controllers/AlunoController.cs
+++ ActionResult/FirstMVC5/Controllers/AlunoController.cs
@@ -14,6 +14,7 @@ namespace FirstMVC5.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         [Route("novo-aluno")]
         public ActionResult NovoAluno(Aluno aluno)
         {
