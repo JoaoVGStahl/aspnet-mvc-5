eb9159584644eda360da103842b7fce4669cd5f8
João Vitor Girardi <Jvitorstahl@hotmail.com>
2021-11-12 08:55:14 -0300

    add Entity Framework to project and created dbcontext

---
+++ ActionResult/FirstMVC5/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FirstMVC5.Models;
+using System.Data.Entity;
+
+namespace FirstMVC5.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+    }
+}
